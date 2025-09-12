6096e1977a2c62556eee9562c0f77d1e54f4c7f7
stormuks <stormuks1@gmail.com>
2025-09-12 16:27:49 +0300

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,63 +1,69 @@
 import '../scss/style.scss'
 
 document.addEventListener('DOMContentLoaded', () => {
-	const burgers = document.querySelectorAll('.header__burger')
 	const body = document.body
+	const header = document.querySelector('header')
+	const menuBtnOpen = document.querySelector('.header__button-open')
+	const scrollBarWidth =
+		window.innerWidth - document.documentElement.clientWidth
 
-	if (burgers.length > 0) {
-		burgers.forEach(burger => {
-			burger.addEventListener('click', () => {
-				body.classList.toggle('menu-open')
-			})
+	const menuOpen = () => {
+		body.classList.add('menu-open')
+		header.style.paddingRight = `${scrollBarWidth}px`
+		body.style.marginRight = `${scrollBarWidth}px`
+	}
+
+	const menuClose = () => {
+		body.classList.remove('menu-open')
+		header.style.paddingRight = ''
+		body.style.marginRight = ''
+	}
+
+	if (menuBtnOpen) {
+		menuBtnOpen.addEventListener('click', () => {
+			menuOpen()
 		})
+
+		body.addEventListener('click', e => {
+			if (body.classList.contains('menu-open') && e.target !== menuBtnOpen) {
+				menuClose()
+			}
+		})
+
 		document.addEventListener('keydown', e => {
 			if (e.key === 'Escape' && body.classList.contains('menu-open')) {
-				body.classList.remove('menu-open')
+				menuClose()
 			}
 		})
 	}
 
-	const header = document.querySelector('.header__wrapper')
-	if (header) {
-		let lastScrollY = 0
-		let ticking = false
+	window.addEventListener('scroll', () => {
+		if (window.scrollY > 0) {
+			header.classList.add('header-scroll')
+		} else {
+			header.classList.remove('header-scroll')
+		}
+	})
 
-		function onScroll() {
-			const currentScroll = window.scrollY
+	const moveGrid = document.querySelector('.footer__grid')
+	const moveBlock = document.querySelector('.footer__block')
+	const moveTitle = moveBlock?.querySelector('.footer__title')
+	const moveImg = moveGrid?.querySelector('.footer__img')
 
-			if (!ticking) {
-				window.requestAnimationFrame(() => {
-					if (currentScroll > 95) {
-						header.classList.add('header-scroll')
-					} else {
-						header.classList.remove('header-scroll')
-					}
-					if (currentScroll < 290) {
-						header.classList.remove('header-show')
-					}
+	function moveTitleFunc() {
+		if (!moveGrid || !moveBlock || !moveTitle || !moveImg) return
 
-					if (currentScroll > lastScrollY) {
-						header.classList.remove('header-show')
-					}
-
-					lastScrollY = currentScroll
-					ticking = false
-				})
-
-				ticking = true
+		if (window.innerWidth <= 768) {
+			if (moveTitle.parentElement === moveBlock) {
+				moveGrid.insertBefore(moveTitle, moveImg)
+			}
+		} else {
+			if (moveTitle.parentElement === moveGrid) {
+				moveBlock.insertBefore(moveTitle, moveBlock.firstChild)
 			}
 		}
-
-		let scrollTimeout
-		window.addEventListener('scroll', () => {
-			onScroll()
-
-			clearTimeout(scrollTimeout)
-			scrollTimeout = setTimeout(() => {
-				if (window.scrollY >= 290) {
-					header.classList.add('header-show')
-				}
-			}, 150)
-		})
 	}
+
+	moveTitleFunc()
+	window.addEventListener('resize', moveTitle)
 })
